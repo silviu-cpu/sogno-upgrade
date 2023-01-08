6224d0804d46e1f92f3459ee8b0a18979fd416e2
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-01-08 20:06:58 +0200

    Added carousel container

--- images/images.js
+++ images/images.js
@@ -1,17 +1,33 @@
-import background from '../public/background.webp'
 import bog from '../public/bog.webp'
 import castelulcorvinilor from '../public/castelulcorvinilor.webp'
-import citadel from '../public/citadel.jpeg'
+import citadel from '../public/citadel.jpg'
 import sublee from '../public/sublee.jpeg'
-import traumer from '../public/traumer.jpeg'
+import traumer from '../public/traumer.jpg'
+import valeaoltului from '../public/valeaoltului.webp'
 
-const images = [
-    {key:1, value: background}, 
-    {key:2, value: bog}, 
-    {ket:3, value: castelulcorvinilor}, 
-    {key:4, value: citadel}, 
-    {key:5, value: sublee}, 
-    {key:6, value: traumer}
-];
+const images = [citadel, castelulcorvinilor,bog,sublee, traumer, valeaoltului];
 
-export default images;
+bog.descriere ='BOg at Palas Garden, Iasi, Romania for Sogno';
+castelulcorvinilor.descriere =`NUSHA at Corvins' Castle,in Romania, for Sogno`;
+citadel.descriere ='Priku b2b Arapu - B/plr. x Citadela (closing) for Sogno';
+sublee.descriere ='Sublee at Cruising Boat x B/plr. in Budapest, for Sogno';
+traumer.descriere ='Traumer at B/plr. x Cruisin (Boat Party Budapest), for Sogno';
+valeaoltului.descriere = 'Catalin Sima at Malaia Lake, Romania, for Sogno';
+
+
+bog.titlu ='BOg';
+castelulcorvinilor.titlu ='NUSHA';
+citadel.titlu ='Priku b2b Arapu';
+sublee.titlu ='Sublee';
+traumer.titlu ='Traumer';
+valeaoltului.titlu = 'Catalin Sima'
+
+bog.link ='https://youtu.be/Kc2Xt_CNerI';
+castelulcorvinilor.link ='https://youtu.be/IuF8s_Vh6ck';
+citadel.link ='https://youtu.be/EZ_Kv5-PiIQ';
+sublee.link ='https://youtu.be/qQnLixgBebk';
+traumer.link ='https://youtu.be/vXd3BCz4DjM';
+valeaoltului.link = 'https://youtu.be/ivoYWV5T20g';
+
+
+export default images
--- pages/events/index.js
+++ pages/events/index.js
@@ -2,24 +2,31 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Navbar from '../../components/navbar'
 import Footer from '../../components/footer'
-import { motion  } from 'framer-motion'
+import { useState } from 'react'
 import images from '../../images/images'
+import { Carousel } from 'react-bootstrap'
 
 export default function Events() {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    }
+
     return (
         <div>
             <Navbar />
-            <div className='App'>
-                <motion.div className='carousel'> 
-                    <motion.div className='inner-carousel'>
-                        {images.map( ({key, value}) => {
-                            return (
-                                <motion.div key={key}><img src={value} alt="" /></motion.div>
-                            );
-                        })}
-                    </motion.div>
-                </motion.div>
-            </div>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                {images.map((item) => (
+                    <Carousel.Item key={item.id} className='itemP' interval={4000}>
+                        <img src={item.src} alt='slides' />
+                        <Carousel.Caption className='caption'>
+                            <h3>{item.titlu}</h3>
+                            <p>{item.descriere}</p>
+                            <a href={item.link} target="_blank" className='btn btn-danger'> View the event </a>
+                        </Carousel.Caption>
+                    </Carousel.Item> 
+                ))}
+            </Carousel>
             <Footer />
         </div>
     )
