a080035b76d48eed82a90264391ce0615ec11227
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-02-27 00:51:57 +0200

    Git add pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
 
     deploy : {
         production : {
-            user : 'ubuntu',
+            user : 'root',
             host : '89.44.137.23',
             ref : 'origin/main',
             repo :'git@github.com:silviu-cpu/sogno-upgrade.git',
-            path : '/home/ubuntu',
+            path : '/home/root',
             'pre-deploy-local': '',
             'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
             'pre-setup': '',
