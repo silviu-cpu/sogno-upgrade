15ab4f0e2309fa0e982bfd531b1b973998cd3fb5
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-03-06 22:05:34 +0200

    Fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,9 @@ module.exports = {
         production : {
             key: 'key.pem',
             user : 'root',
-            host : process.env.DB_HOST,
+            host : "89.44.137.23",
             ref : 'origin/main',
-            repo :process.env.DB_REPO,
+            repo : "git@github.com:silviu-cpu/sogno-upgrade.git",
             path : '/home/root',
             'pre-deploy-local': '',
             'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
